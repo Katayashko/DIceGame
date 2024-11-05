59c3d0e972a20f10490aa35999a93ab323eefce9
Katayashko <kajaniew1@gmail.com>
2024-11-05 16:03:39 +0100

    5listopada

--- app/src/main/java/com/example/dicegame/MainActivity.java
+++ app/src/main/java/com/example/dicegame/MainActivity.java
@@ -4,18 +4,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView titleTV, numTV1, numTV2, numTV3, numTV4, numTV5, thisResultTV, gameResultTV, numToss;
-    private Button tossBtn, resetBtn;
+    private TextView numTV1, numTV2, numTV3, numTV4, numTV5, thisResultTV, gameResultTV, numToss;
 
 
     @Override
@@ -24,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        titleTV = findViewById(R.id.titleTV);
         numTV1 = findViewById(R.id.numTV1);
         numTV2 = findViewById(R.id.numTV2);
         numTV3 = findViewById(R.id.numTV3);
@@ -33,41 +27,80 @@ public class MainActivity extends AppCompatActivity {
         thisResultTV = findViewById(R.id.thisResultTV);
         gameResultTV = findViewById(R.id.gameResultTV);
         numToss = findViewById(R.id.numToss);
-        tossBtn = findViewById(R.id.tossBtn);
-        resetBtn = findViewById(R.id.resetBtn);
+        Button tossBtn = findViewById(R.id.tossBtn);
+        Button resetBtn = findViewById(R.id.resetBtn);
+
 
         tossBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                rollDice();
+                int newScore = rollDice();
+                updateScore(newScore);
+                updateRollCount();
+                displayDiceResults(tvNum);
+            }
+        });
+        resetBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                resetGame();
             }
         });
 
     }
 
-    public void rollDice(){
+    Random r = new Random();
+    int tossNum = 0;
+    int score = 0;
+    int[] tvNum = new int[6];
+
+    private int rollDice(){
         int resultThis = 0;
-        int resultGame = 0;
-        Random r = new Random();
-        int[] tvNum = new int[5];
+
+        tvNum[5] = 7;
         for(int i = 0; i < 5; i++){
             tvNum[i] = r.nextInt(6);
         }
-        numTV1.setText(tvNum[0]);
-        numTV2.setText(tvNum[1]);
-        numTV3.setText(tvNum[2]);
-        numTV4.setText(tvNum[3]);
-        numTV5.setText(tvNum[4]);
 
 
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 5; j++){
-                if(tvNum[i] == tvNum[j]){
-                    resultThis += tvNum[j];
+        for (int i = 0; i < 5; i++) {
+            for (int j = i; j < 5; j++) {
+                if (tvNum[i] == tvNum[j+1]) {
+                    resultThis += tvNum[i]*2;
                 }
             }
         }
-        thisResultTV.setText(resultThis);
 
+        thisResultTV.setText("Wynik tego losowania: " + resultThis);
+        return resultThis;
+    }
+
+    private void resetGame(){
+        numTV1.setText(" ? ");
+        numTV2.setText(" ? ");
+        numTV3.setText(" ? ");
+        numTV4.setText(" ? ");
+        numTV5.setText(" ? ");
+        thisResultTV.setText("Wynik tego losowania: 0");
+        gameResultTV.setText("Wynik gry: 0");
+        numToss.setText("Liczba rzutów: 0");
+    }
+
+    private void updateScore(int newScore){
+        score += newScore;
+        gameResultTV.setText("Wynik gry: " + score);
+    }
+
+    private void updateRollCount(){
+        tossNum++;
+        numToss.setText("Liczba rzutów: " + tossNum);
+    }
+
+    private void displayDiceResults(int[] diceResults){
+        numTV1.setText(" " + diceResults[0] + " ");
+        numTV2.setText(" " + diceResults[1] + " ");
+        numTV3.setText(" " + diceResults[2] + " ");
+        numTV4.setText(" " + diceResults[3] + " ");
+        numTV5.setText(" " + diceResults[4] + " ");
     }
 }
